3d2542bf01af9017e14c9fb47261ecdaf85212c0
EROSENNIN69 <sobitgrg300@gmail.com>
2025-02-12 18:31:37 +0545

    some changes

--- src/Components/Header/header.component.jsx
+++ src/Components/Header/header.component.jsx
@@ -15,38 +15,36 @@ const HeaderComponent = () => {
   };
 
   return (
-    <div className="App">
-      <div className="Header">
-        <div className="Container">
-          <nav>
-            <img src={logo} alt="Logo" className="Logo" />
-            <ul id="sideMenu" className={menuOpen ? "open" : ""}>
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#services">Services</a>
-              </li>
-              <li>
-                <a href="#portfolio">Portfolio</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-              <i className="fa-solid fa-xmark" onClick={closeMenu}></i>
-            </ul>
-            <i className="fa-solid fa-bars" onClick={openMenu}></i>
-          </nav>
-          <div className="header-text">
-            <p>Hope/Mazzystar_fanclub</p>
-            <h1>
-              Hi, I'm <span>Himal</span>. <br></br>I am an artist and <br></br>{" "}
-              a photographer.
-            </h1>
-          </div>
+    <div className="header">
+      <div className="container">
+        <nav>
+          <img src={logo} alt="Logo" className="logo" />
+          <ul id="sideMenu" className={menuOpen ? "open" : ""}>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <i className="fa-solid fa-xmark" onClick={closeMenu}></i>
+          </ul>
+          <i className="fa-solid fa-bars" onClick={openMenu}></i>
+        </nav>
+        <div className="header-text">
+          <p>Hope/Mazzystar_fanclub</p>
+          <h1>
+            Hi, I'm <span>Himal</span>. <br></br>I am an artist and <br></br> a
+            photographer.
+          </h1>
         </div>
       </div>
     </div>
